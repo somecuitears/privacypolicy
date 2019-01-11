bda73b2f32162390d850dc4724a81335cb295a7e
msumeet <msumeetiw@gmail.com>
2019-01-11 10:35:25 +0545

    Updates

---
+++ precache-manifest.21d9a4b3bb6f77b4b945c1b85a4a9fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/privacypolicy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc699ba949fccb897e18",
+    "url": "/privacypolicy/static/js/runtime~main.bc699ba9.js"
+  },
+  {
+    "revision": "e7d62e3a09e0cdbe9595",
+    "url": "/privacypolicy/static/js/main.e7d62e3a.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/privacypolicy/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "e7d62e3a09e0cdbe9595",
+    "url": "/privacypolicy/static/css/main.a71d718b.chunk.css"
+  },
+  {
+    "revision": "e600bbde57e62712d1f77f78ee6adc73",
+    "url": "/privacypolicy/index.html"
+  }
+];
